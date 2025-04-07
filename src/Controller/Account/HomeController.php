4bfae7c3b2bf25fa1dde609a6cf55f8b06df779c
Bonnegent <richard.bonnegent@gmail.com>
2025-04-08 01:08:05 +0200

    Ajout de la gestion des commentaires de l'utilisateur : mise à jour de HomeController pour récupérer et afficher les commentaires récents, création d'un nouveau template all_comments.html.twig pour afficher tous les commentaires de l'utilisateur.

--- src/Controller/Account/HomeController.php
+++ src/Controller/Account/HomeController.php
@@ -2,14 +2,38 @@
 
 namespace App\Controller\Account;
 
+use App\Repository\CommentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController{
     #[Route('/compte', name: 'app_account')]
-    public function index(): Response
+    public function index(CommentRepository $commentRepository): Response
     {
-        return $this->render('account/index.html.twig');
+        $user = $this->getUser(); // Récupérer l'utilisateur connectéRécupérer les commentaires de l'utilisateur
+        $comments = $commentRepository->findBy(
+            ['author' => $user], // Filtrer par utilisateur connecté
+            ['createdAt' => 'DESC'], // Trier par date décroissante
+            5 // Limiter à 5 commentaires
+        );
+
+        return $this->render('account/index.html.twig', [
+            'comments' => $comments,
+        ]);
+    }
+
+    #[Route('/compte/commentaires', name: 'app_account_all_comments')]
+    public function allComments(CommentRepository $commentRepository): Response
+    {
+        $user = $this->getUser(); // Récupérer l'utilisateur connecté
+        $comments = $commentRepository->findBy(
+            ['author' => $user], // Filtrer par utilisateur connecté
+            ['createdAt' => 'DESC'] // Trier par date décroissante
+        );
+
+        return $this->render('account/all_comments.html.twig', [
+            'comments' => $comments,
+        ]);
     }
 }
--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -39,5 +39,5 @@ class CommentRepository extends ServiceEntityRepository
     //            ->getQuery()
     //            ->getOneOrNullResult()
     //        ;
-    //    }
+    //    }tResult();
 }
